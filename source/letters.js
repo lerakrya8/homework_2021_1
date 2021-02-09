c393fb8d43744f3415817a479f25a0237781370e
lerakry8 <lerakrya8@gmail.com>
2021-02-09 23:47:26 +0300

    Еще больше упрощений

--- source/letters.js
+++ source/letters.js
@@ -1,5 +1,17 @@
 'use strict';
 /***
+ * Функция находит символы, которые встречаются больше 1 раза
+ *
+ * @param {String} acc - аккумулятор - строка с повторяющимися буквами
+ * @param {String} item - текущая буква
+ * @param {Number} idx - текущий номер элемента
+ * @param {Array} arr - массив, по которому идет обход
+ * @returns {String}
+ */
+
+const findRepeatLetters = (acc, item, idx, arr) => arr.slice(0, idx).includes(item) ? acc + item : acc;
+
+/***
  * Функция удаляет символы, которые встречаются больше 1 раза
  *
  * @param {String} str - начальная строка
@@ -12,10 +24,9 @@ const letters = (str, entryType) => {
          return;
     }
     if (entryType === undefined) {
-        const meetLetters = str.split('').reduce(
-            (acc, letter, letterIndex, currentStr) => currentStr.slice(0, letterIndex).includes(letter) ? acc + letter : acc, '');
-        const expr = new RegExp(`[${[...new Set(meetLetters)].map(letter => letter + '+').join('')}]`,'g');
-        return str.replace(expr, '');
+        const repeatLetters = str.split('').reduce((acc, letter, letterIndex, currentStr) => reducer(acc, letter, letterIndex, currentStr), '');
+        const deleteRepeatLetters = new RegExp(`[${[...new Set(repeatLetters)].map(letter => letter + '+').join('')}]`,'g');
+        return str.replace(deleteRepeatLetters, '');
     }
     return entryType ? [...new Set(str)].join('') : [...new Set([...str].reverse())].reverse().join('');
 }
